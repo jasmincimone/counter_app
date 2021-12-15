abaa5a31a7e0f1cbeb43252b96f3ca035ad5d0fa
Jasmin Cimone <jasmincimone@Jasmins-MacBook-Air.local>
2021-12-15 09:30:23 -0500

    2nd Commit

---
+++ functions.js
@@ -0,0 +1,57 @@
+/* 
+1. Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
+*/
+
+maxOfTwoNumbers = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  maxOfThree();
+  
+  /*
+  2. Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+  */
+  maxOfThree = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  /*
+  3. Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+  */
+  isCharacterAVowel = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  /*
+  4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+  */
+  
+  sumArray = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  /*
+  5.Write a function that returns the number of arguments passed to the function when called.
+  */
+  
+  /*
+  6. Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+  */
+  
+  reverseString = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  /*
+  7. Write a function findLongestWord that takes an array of words and returns the length of the longest one.
+  */
+  findLongestWord = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  /*
+  8. Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
+  */
+  filterLongWords = () => {
+    // ADD YOUR CODE HERE
+  };
